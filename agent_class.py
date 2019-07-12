fixed problem with gradients

agent_class.py
@@ -62,7 +62,7 @@ class Agent:
 
         # Create the operation to update gradients with the gradients placeholder...
 
-        optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
+        optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
         # Update gradients operation applies gradients that were fed into the corresponding trainable vars in the model
         # Operation runs every time model needs to appy what it has learned from its games and update its parameters
         self.update_gradients = optimizer.apply_gradients(zip(self.gradients_to_apply, tf.trainable_variables()))

agent_main.py
@@ -28,184 +28,151 @@ def discount_normalize_rewards(rewards):
     return discounted_rewards
 
 
-tf.reset_default_graph()
+def neural_network_loop():
+    tf.reset_default_graph()
 
-env = gc.GameMethods()
+    env = gc.GameMethods()
 
-num_actions = 4
-state_size = 2
+    num_actions = 4
+    state_size = 2
 
-index = 1
+    index = 1
 
-path = "./text_adventure_{}/".format(index)  # for checkpoints
+    path = "./text_adventure_{}/".format(index)  # for checkpoints
 
-training_episodes = 100000  # 1000
-max_steps_per_episode = 1000  # 5000
-episode_batch_size = 5
+    training_episodes = 100000  # 1000
+    max_steps_per_episode = 1000  # 5000
+    episode_batch_size = 5
 
-agent = Agent(num_actions, state_size)
+    agent = Agent(num_actions, state_size)
 
-init = tf.global_variables_initializer()
+    init = tf.global_variables_initializer()
 
-saver = tf.train.Saver(max_to_keep=2)
+    saver = tf.train.Saver(max_to_keep=2)
 
-if not os.path.exists(path):
-    os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-with tf.Session() as sess:
-    sess.run(init)
+    with tf.Session() as sess:
+        sess.run(init)
 
-    total_episode_rewards = []
+        total_episode_rewards = []
 
-    gradient_buffer = sess.run(tf.trainable_variables())
+        gradient_buffer = sess.run(tf.trainable_variables())
 
-    for index, gradient in enumerate(gradient_buffer):
-        gradient_buffer[index] = gradient * 0
+        for index, gradient in enumerate(gradient_buffer):
+            gradient_buffer[index] = gradient * 0
 
-    for episode in range(training_episodes):
-        state_all = env.reset()
+        for episode in range(training_episodes):
+            state_all = env.reset()
 
-        state = state_all[3]
-
-        state = np.reshape(state, [1, state_size])
-
-        agent.num_actions = 3
-
-        episode_history = []
-        episode_rewards = 0
-
-        for step in range(max_steps_per_episode):
-
-            if (episode % 5000 == 0) and (episode is not 0):
-                print("Currently on episode " + str(episode))
-                training_current = env.render("on")
-            else:
-                training_current = env.render("off")
-
-            if (step % 1000 == 0) and (step is not 0):
-                print("Currently on step " + str(step))
-
-            # current = training_current
-            # locations = []
-            # for j in gl.locations[current]["direction_values"]:
-            #     locations.append(j)
-
-            locations = []
-            for i in gl.directions:
-                locations.append(i)
-
-            locations = np.array(locations)
-            # print("locations: {0}, shape: {1}".format(str(locations), str(locations.shape)))
-            # locations = np.reshape(locations, [1, 4])
-
-            action_probabilities = sess.run(agent.outputs, feed_dict={agent.input_layer: [state]})
-
-            action_choice = np.random.choice(locations, p=action_probabilities[0])
-
-            # Save the resulting states, rewards and whether the episode finished
-            state_next, reward, done, _ = env.step(action_choice)
-
-            episode_history.append([state, action_choice, reward, state_next])
-            state = state_next
+            state = state_all[3]
 
             state = np.reshape(state, [1, state_size])
 
-            # print("state: {}, state shape: {}".format(str(state), str(state.shape)))
+            agent.num_actions = 3
 
-            episode_rewards += reward
+            episode_history = []
+            episode_rewards = 0
 
-            if done or step + 1 == max_steps_per_episode:
-                total_episode_rewards.append(episode_rewards)
-                episode_history = np.array(episode_history)
+            for step in range(max_steps_per_episode):
 
-                # normalize rewards fn on the stored rewards in episode history
-                episode_history[:, 2] = discount_normalize_rewards(episode_history[:, 2])
+                if (episode % 5000 == 0) and (episode is not 0):
+                    print("Currently on episode " + str(episode))
+                    training_current = env.render("on")
+                else:
+                    training_current = env.render("off")
 
-                # print("episode_history: \n{},\nformated: \n{},\nshape: \n{}\n".format(episode_history, episode_history[:, 2], episode_history[:, 2].shape))
-                eh0 = episode_history[:, 0]
-                # print("[1] eh0: {}, eh0 shape: {}".format(eh0, eh0.shape))
-                # eh0_list = []
-                # for i in range(eh0.size):
-                #     eh0_list.append(eh0[i])
-                # print("[2] eh0: {}, eh0 shape: {}".format(eh0_list, eh0.shape))
-                eh0 = np.reshape(eh0, [eh0.size, 1])
-                # eh0 = np.reshape(eh0, [eh0.size, 1, 2])
-                # print("[3] eh0: {}, eh0 shape: {}".format(eh0, eh0.shape))
-                # print("Shape: " + str(episode_history[:, 0].shape))
-                eh2 = []
-                # print(eh2[0][0])
-                for i in range(episode_history[:, 2].size):
-                    eh2.append(episode_history[:, 2][i][0])
+                if (step % 1000 == 0) and (step is not 0):
+                    print("Currently on step " + str(step))
 
-                eh2 = np.array(eh2)
-                eh2 = np.resize(eh2, [eh2.size, ])
+                locations = []
+                for i in gl.directions:
+                    locations.append(i)
 
-                eh1 = episode_history[:, 1]
-                # eh2 = episode_history[:, 2]
-                eh1_list = []
-                types = []
-                for i in range(eh1.size):
-                    eh1_list.append(int(eh1[i]))
-                    types.append(type(eh1[i]))
-                eh1 = np.reshape(eh1, [eh1.size, ])
-                # print("eh0: {}, eh0 shape: {}".format(eh0[0][0], eh0.shape))
-                # print("eh1: {}, shape: {}, type: {}".format(str(eh1), str(eh1.shape), types))
-                # print("eh2: {}, eh2 shape: {}".format(eh2, eh2.shape))
+                locations = np.array(locations)
 
-                # ep_gradients = sess.run(agent.gradients,
-                #                         feed_dict={agent.input_layer: np.vstack(eh0),
-                #                                    agent.actions: eh1,
-                #                                    agent.rewards: eh2})
+                action_probabilities = sess.run(agent.outputs, feed_dict={agent.input_layer: [state]})
 
-                # # add the gradients
-                # for index, gradient in enumerate(ep_gradients):
-                #     gradient_buffer[index] += gradient
+                action_choice = np.random.choice(locations, p=action_probabilities[0])
 
-                break
-        if episode % episode_batch_size == 0:
-            feed_dict_gradients = dict(zip(agent.gradients_to_apply, gradient_buffer))
+                # Save the resulting states, rewards and whether the episode finished
+                state_next, reward, done, _ = env.step(action_choice)
 
-            sess.run(agent.update_gradients, feed_dict=feed_dict_gradients)
+                episode_history.append([[state], action_choice, reward, state_next])
+                state = state_next
 
-            for index, gradient in enumerate(gradient_buffer):
-                gradient_buffer[index] = gradient * 0
+                state = np.reshape(state, [1, state_size])
 
-            if episode % 100 == 0:
-                saver.save(sess, path + "pg-checkpoint", episode)
+                # print("state: {}, state shape: {}".format(str(state), str(state.shape)))
 
-                print("Average reward / 100 eps: " + str(np.mean(total_episode_rewards[-100:])))
+                episode_rewards += reward
 
-print("Now Testing neural network... \n")
-testing_episodes = 2
+                if done or step + 1 == max_steps_per_episode:
+                    total_episode_rewards.append(episode_rewards)
+                    episode_history = np.array(episode_history)
 
-with tf.Session() as sess:
-    checkpoint = tf.train.get_checkpoint_state(path)
-    saver.restore(sess, checkpoint.model_checkpoint_path)
+                    # normalize rewards fn on the stored rewards in episode history
+                    episode_history[:, 2] = discount_normalize_rewards(episode_history[:, 2])
 
-    for episode in range(testing_episodes):
+                    eh0 = episode_history[:, 0]
+                    eh1 = episode_history[:, 1]
+                    eh2 = episode_history[:, 2]
 
-        state_all = env.reset()
+                    ep_gradients = sess.run(agent.gradients,
+                                            feed_dict={agent.input_layer: np.vstack(eh0),
+                                                       agent.actions: eh1,
+                                                       agent.rewards: eh2})
 
-        state = state_all[3]
+                    # add the gradients
+                    for index, gradient in enumerate(ep_gradients):
+                        gradient_buffer[index] += gradient
 
-        state = np.reshape(state, [1, state_size])
+                    break
+                if episode % episode_batch_size == 0 and step == 1:
+                    feed_dict_gradients = dict(zip(agent.gradients_to_apply, gradient_buffer))
 
-        episode_rewards = 0
+                    sess.run(agent.update_gradients, feed_dict=feed_dict_gradients)
 
-        for step in range(max_steps_per_episode):
-            env.render("on")
+                    for index, gradient in enumerate(gradient_buffer):
+                        gradient_buffer[index] = gradient * 0
 
-            # Get Action
-            action_argmax = sess.run(agent.choice, feed_dict={agent.input_layer: [state]})
-            action_choice = action_argmax[0]
+                    if episode % 2000 == 0:
+                        saver.save(sess, path + "pg-checkpoint", episode)
 
-            state_next, reward, done, _ = env.step(action_choice)
-            state = state_next
+                        print("Average reward / 2000 eps: " + str(np.mean(total_episode_rewards[-2000:])))
 
-            episode_rewards += reward
+    print("Now Testing neural network... \n")
+    testing_episodes = 2
 
-            if done or step + 1 == max_steps_per_episode:
-                print("Rewards for episode " + str(episode) + ": " + str(episode_rewards))
+    with tf.Session() as sess:
+        checkpoint = tf.train.get_checkpoint_state(path)
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+
+        for episode in range(testing_episodes):
+
+            state_all = env.reset()
+
+            state = state_all[3]
+
+            state = np.reshape(state, [1, state_size])
+
+            episode_rewards = 0
+
+            for step in range(max_steps_per_episode):
+                env.render("on")
+
+                # Get Action
+                action_argmax = sess.run(agent.choice, feed_dict={agent.input_layer: [state]})
+                action_choice = action_argmax[0]
+
+                state_next, reward, done, _ = env.step(action_choice)
+                state = state_next
+
+                episode_rewards += reward
+
+                if done or step + 1 == max_steps_per_episode:
+                    print("Rewards for episode " + str(episode) + ": " + str(episode_rewards))
                 break
 
 

game_classes.py
@@ -17,6 +17,7 @@ class Player:
         self.moves = 0
         self.has_chest = False
         self.is_dead = False
+        self.max_moves = 30
 
     def view_health(self):
         health = str(self.health)
@@ -66,12 +67,12 @@ class Program:
         self.encountered = 0
         self.render = False
         self.reward = 0.0
-        self.max_moves = 30
+        self.max_moves = self.player.max_moves
         self.has_chest = 0
         self.has_won = 0
 
     def obtained_chest(self):
-        if self.current_location == 12:
+        if self.current_location == 12 and self.player.has_chest is False:
             text = "You have obtained the chest, hurry to the Coffee Shop! \n "
             self.render_text(text)
             self.player.obtains_chest()
@@ -79,7 +80,7 @@ class Program:
             self.has_chest = 1
 
     def player_won(self):
-        if (self.has_chest == 0) and (self.current_location == 11) and (self.player.view_health() is not 0):
+        if (self.has_chest == 0) and (self.current_location == 11) and (self.player.view_health_num() > 0):
             text = "You Win! \n "
             self.render_text(text)
             self.reward *= 10.0
@@ -125,10 +126,8 @@ class Program:
 
     def user_direction(self, action):
         if not self.render:
-            # player_option = input("Direction: ").upper()
             player_option = action
         else:
-            # player_option = input().upper()
             player_option = action
             text = "Directon: {}".format(player_option)
             self.render_text(text)
@@ -213,6 +212,7 @@ class Program:
         if health_bool is True:
             self.encountered = 1
             self.player.effect_health(-1)
+            self.reward -= 2.0
             passed = False
 
         else:
@@ -234,13 +234,14 @@ class Program:
         things_encountered = self.location_health()
         health = self.player.view_health()
         moves = self.player.view_moves()
+        rewards = int(self.reward)
 
         if health == "0":
             self.loop_break = True
 
-        text = ("-" * 46)
-        text += "\n| Encountered: {0}, Health: {1:2}, Moves: {2:2} |\n".format(things_encountered, health, moves)
-        text += ("-" * 46)
+        text = ("-" * 59)
+        text += "\n| Encountered: {0}, Health: {1:2}, Moves: {2:2}, Rewards: {3:2} |\n".format(things_encountered, health, moves, rewards)
+        text += ("-" * 59)
         text += " \n"
 
         self.render_text(text)
@@ -262,7 +263,7 @@ class Program:
             self.reward //= 2
             self.loop_break = True
 
-        # return self.loop_break
+        return self.loop_break
 
     def render_text(self, text):
         if self.render:
@@ -280,6 +281,7 @@ class GameMethods:
         self.game.current_location = 0
         self.game.encountered = 0
         self.game.loop_break = False
+        self.game.reward = 0.0
 
         observations = np.array([self.game.encountered, self.game.has_chest])
 
@@ -299,7 +301,7 @@ class GameMethods:
 
         action = gl.directions[action]["short"]
 
-        encountered, health, moves, available_directions = gm.main(action, self.game)
+        encountered, health, moves, available_directions = gm.machine_loop(action, self.game)
 
         token = self.game.max_moves - int(moves)
         self.game.reward += token
@@ -313,6 +315,161 @@ class GameMethods:
 
     def close(self):
         self.game.loop_break = True
+
+
+class User:
+
+    def __init__(self):
+        self.player = Player()
+        self.current_location = 0
+        self.loop_break = False
+        self.reward = 0
+        self.max_moves = self.player.max_moves
+
+    def main(self):
+        pass
+
+    def init_play(self):
+        name = input("\nPlease enter your name: ")
+        self.player = Player(name)
+
+        self.intro()
+
+    def intro(self):
+        print(
+            "\nWelcome {0.player.name}, the objective is to obtain a chest of gold and take it "
+            "to the Grand Master at the Coffee Shop".format(self))
+        print("You must do so while trying to avoid goblins, they hide in different locations, "
+              "and constantly are on the move.")
+        print("If a goblin sees you, you lose a some health, with {0} healths to start with.".format(
+            self.player.view_health()))
+        print("However, if a doctor happens to be at that location, you can gain a health.")
+        print("Good luck!\n")
+
+    def print_locs(self):
+        # print(self.locations_print
+        text = self.locations_print()
+        print(text)
+
+        self.user_direction()
+
+    def locations_print(self):
+        global locations
+        location = locations[self.current_location]["name"]
+
+        text = "You are currently next to a {0}, you can go ".format(location)
+        for i in range(0, len(locations[self.current_location]["locations"])):
+            if locations[self.current_location]["locations"][i] is not "Q":
+                if (i + 2) < (len(locations[self.current_location]["locations"])):
+                    text += str(locations[self.current_location]["locations"][i]) + ", "
+                elif (i + 2) == (len(locations[self.current_location]["locations"])):
+                    text += str(locations[self.current_location]["locations"][i])
+
+        return text
+
+    def user_direction(self):
+        player_option = input("Direction: ").upper()
+        player_option = self.direction_query(player_option)
+
+        if player_option is "Q":
+            print("Have a good day!")
+            print()
+            self.loop_break = True
+
+        else:
+            self.current_location = self.advance_location(player_option)  # moves to next location
+            self.player.add_move()
+
+    def direction_query(self, user_input):
+        available_keys = []
+        available_directions = []
+        problems = False
+
+        for i in gl.directions.values():
+            available_keys.append(i["short"])
+            available_keys.append(i["long"])
+
+        for i in gl.locations[self.current_location]["locations"]:
+            available_directions.append(i)
+
+        if user_input in available_keys:
+            for i in range(0, len(gl.directions)):
+                if user_input in gl.directions[i].values():
+                    user_input = gl.directions[i]["short"]
+                    break
+
+        if user_input not in available_keys:
+            problems = True
+
+        if (user_input in available_keys) and (user_input not in available_directions):
+            problems = True
+
+        if problems is True:
+            while problems is True:
+                self.reward //= 2
+                user_input = input("Please enter a valid direction: ").upper()
+                if user_input in available_keys:
+                    for i in range(0, len(gl.directions)):
+                        if user_input in gl.directions[i].values():
+                            user_input = gl.directions[i]["short"]
+                            break
+                    problems = False
+                    if user_input in available_directions:
+                        problems = False
+                    else:
+                        problems = True
+                else:
+                    problems = True
+        else:
+            self.reward += 1
+
+        return user_input
+
+    def advance_location(self, direction):
+        global locations
+        move_to = locations[self.current_location]["directions"][direction]
+        return move_to
+
+    def location_health(self):
+        passed = True
+        max_num = locations[self.current_location]["death"]
+
+        health_bool = random_num(max_num)
+        text = "Nobody"
+        if health_bool is True:
+            text = "Goblin"
+            self.player.effect_health(-1)
+            self.reward -= 2
+            passed = False
+
+        else:
+            max_num = locations[self.current_location]["heal"]
+            health_bool = random_num(max_num)
+            if health_bool is True:
+                text = "Doctor"
+                self.player.effect_health(1)
+                self.reward += 2
+
+        if passed:
+            self.reward += 1
+
+        return text
+
+    def encountered_stats(self):
+        did_encounter = self.location_health()
+        health = self.player.view_health()
+        moves = self.player.view_moves()
+        rewards = self.reward
+
+        if health == "0":
+            self.loop_break = True
+
+        text = ("-" * 59)
+        text += "\n| Encountered: {0}, Health: {1:2}, Moves: {2:2}, Rewards: {3:2} |\n"\
+            .format(did_encounter, health, moves, rewards)
+        text += ("-" * 59)
+        print(text)
+        print()
 
 
 def random_num(max_range):